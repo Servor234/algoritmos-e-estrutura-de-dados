df0f77000c65f78278bef3f70fbc4abc4e9bbd1a
Servor234 <teixeirarenato603@gmail.com>
2026-03-05 07:29:24 -0300

    Create circulo.c

---
+++ circulo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float r, p, a;
+
+    printf("qual seu raio?\n");
+    scanf("%f", &r);
+
+    p=2*3.14*r;
+    a=r*r*3.14;
+
+    printf("%.2f %.2f", a, p);
+    return 0;
+
+}
