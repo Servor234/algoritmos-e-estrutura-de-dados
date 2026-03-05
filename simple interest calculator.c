1ad510f5b2ba2db8bb80d993304f0ed920782c67
Servor234 <teixeirarenato603@gmail.com>
2026-03-05 07:44:28 -0300

    Create simple interest calculator.c

---
+++ simple interest calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float d, i, g, r;
+
+    printf("what is your deposit?\n");
+    scanf("%f", &d);
+
+    printf("what is the interest rate?\n");
+    scanf("%f", &i);
+
+    g=d*(i/100);
+    r=g+d;
+
+    printf("\n\tyour deposit increased in %.2f\n\tyour new total is %.2f", g, r);
+
+}
