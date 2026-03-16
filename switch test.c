4d014bc276c33ff888bf67b74c39049f04d298d6
Servor234 <teixeirarenato603@gmail.com>
2026-03-16 13:46:38 -0300

    Create switch test.c

---
+++ switch test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a,b,c,d;
+
+    printf("type two numbers\n");
+    scanf("%d %d", &a, &b);
+
+    printf("choose an operation\n\t1 Sum\n\t2 Sub\n\t3 Multi\n\t4 Divi\n");
+    scanf("%d", &c);
+
+    switch(c){
+    case 1:
+        d=a+b;
+        printf("%d\n", d);
+        break;
+
+    case 2:
+        abs(d=a-b);
+        printf("%d\n", d);
+        break;
+
+    case 3:
+        d=a*b;
+        printf("%d\n", d);
+        break;
+
+    case 4:
+        d=a/b;
+        printf("%d\n", d);
+        break;
+        }
+
+    }
