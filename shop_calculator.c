24a9dc2a6df4330c0a7d0f439444f8e2593351bc
Servor234 <teixeirarenato603@gmail.com>
2026-03-23 14:14:35 -0300

    Refactor shop_calculator to simplify product total calculation
    Removed unused variables and calculations related to min, max, and product count.

--- shop_calculator.c
+++ shop_calculator.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <limits.h>
 
 int main(){
-    int x, y, z, ma=0, me=INT_MAX;
+    int x, y, i;
 
     for(int i=0; i<=1;){
         printf("how much is your product?\n");
@@ -10,18 +9,7 @@ int main(){
         y=x+y;
         printf("do you want to continue?\n\t1 for yes\n\t2 for no\n");
         scanf("%d", &i);
-        z++;
-        if(x<me){
-            me=x;
-        }
-        if(x>ma){
-            ma=x;
-        }
     }
-    printf("your total %d\n", y);
-    printf("you have %d products\n", z);
-    printf("the average price is %d\n", y/z);
-    printf("the smallest value is %d\n", me);
-    printf("the biggest value is %d\n", ma);
+    printf("your total %d", y);
 
 }
