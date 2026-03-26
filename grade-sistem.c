34b892726e9d3001c8b736789eb8891fbab5e21d
Servor234 <teixeirarenato603@gmail.com>
2026-03-26 08:04:32 -0300

    Update grade-sistem.c

--- grade-sistem.c
+++ grade-sistem.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <limits.h>
 
 int main(){
-    int x=1,ma=INT_MAX,me=0;
-    int z,y=0,t;
+    int x=1,ma=0,me=200000;
+    int z,y=0;
+    float t;
 
     while(x>-1){
             printf("type a grade\n");
@@ -11,17 +11,17 @@ int main(){
             if(x>-1){
                 y++;
                 z=z+x;
-                if(x>me)
-                    me=x;
-                else if(x<ma)
+                if(x>ma)
                     ma=x;
+                if(x<me)
+                    me=x;
 
             }
         }
     t=z/y;
     printf("the number of students is %d\n", y);
-    printf("the average grade is %d\n", t);
+    printf("the average grade is %f\n", t);
     printf("the greatest grade is %d and the smallest is %d\n", ma,me);
-    
+
     return 0;
     }
