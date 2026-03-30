f0cf812385478a0c635b461ff8efbef7b1be1057
Servor234 <teixeirarenato603@gmail.com>
2026-03-30 11:05:13 -0300

    Implement division using repeated subtraction

---
+++ division.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int x=0,y=0,z=0,h;
+    
+    printf("type 2 numbers to divide\n");
+    scanf("%d %d", &x, &y);
+    
+    while(x>=y){
+        x=x-y;
+        z++;
+    }
+    h=x;
+    
+    printf("%d e resto: %d", z, h);
+    
+    return 0;
+  
+    
+}
