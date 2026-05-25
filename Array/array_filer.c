ac2f267da114d572765e5680d51707cd24bd99f8
Servor234 <teixeirarenato603@gmail.com>
2026-05-25 16:12:53 -0300

    Create array_filer.c

---
+++ Array/array_filer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a[6];
+
+    for(int i=0;i<6;i++){
+        scanf("%d", &a[i]);
+    }
+
+    for(int i=0;i<6;i++){
+        printf("%d\n", a[i]);
+    }
+}
