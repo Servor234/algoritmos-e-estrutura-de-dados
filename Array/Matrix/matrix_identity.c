258afed4e3058fdca038185abc1e102c176c3d39
Servor234 <teixeirarenato603@gmail.com>
2026-05-25 20:28:00 -0300

    Add matrix_identity.c for identity matrix generation
    Implement a program to generate a 5x5 identity matrix.

---
+++ Array/Matrix/matrix_identity.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a[5][5]={0};
+
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            if(i==j){
+                a[i][j]=1;
+            }
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
