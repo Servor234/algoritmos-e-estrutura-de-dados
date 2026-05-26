82fda9c1ea59520c98d813c0732a7b506c82fcaf
Servor234 <teixeirarenato603@gmail.com>
2026-05-25 21:22:57 -0300

    Add matrix_under_triangular.c for matrix handling
    Implement matrix input and output with under triangular condition

---
+++ Array/Matrix/matrix_under_triangular.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int a[4][4];
+
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            scanf("%d", &a[i][j]);
+            if(a[i][j]>20 || a[i][j]<0){
+                scanf("%d", &a[i][j]);
+            }
+            if(j>i){
+                a[i][j]=0;
+            }
+        }
+    }
+
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
