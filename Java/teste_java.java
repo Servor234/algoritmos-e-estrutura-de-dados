1575306a04464a303cfd983a9626d1f13d3706e2
Renato Teixeira Miranda Guimaraes <Teixeirarenato603@gmail.com>
2026-08-06 12:14:57 -0300

    criado arquivo de soma simples em java

--- Java/teste_java.java
+++ Java/teste_java.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class teste_java {
-    public static void main() {
-        System.out.println("Hello, World!");
+    public static void main (String[] args) {
+        Scanner input = new Scanner (System.in);
+        int a=0;
+
+        System.out.print("Digite primeiro numero: ");
+        a= input.nextInt();
+
+        int b=0;
+
+        System.out.print("Digite outro numero: ");
+        b= input.nextInt();
+
+        int soma=a+b;
+
+        System.out.printf("Resultado: %d\n", soma);
+
+        System.out.println("Hello, World!, carros voam as tres da manha");
+        System.out.println("bom dia");
+
+        input.close();
+
     }
 }
