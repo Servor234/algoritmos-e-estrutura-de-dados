f6bb58bbd86253dcbbd9acab62c9a2fbdbc386c3
Renato Teixeira Miranda Guimaraes <Teixeirarenato603@gmail.com>
2026-08-06 12:23:35 -0300

    começo projeto calculadora

---
+++ Java/Calculadora.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Calculadora{
+    public static void main(){
+        Scanner scanner = new Scanner(System.in);
+
+        int a=0,b=0, op=0;
+
+        
+
+    }
+}
