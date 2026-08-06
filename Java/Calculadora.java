42b153694a86b7d90dcc0e4b6928fbf84f874276
Renato Teixeira Miranda Guimaraes <Teixeirarenato603@gmail.com>
2026-08-06 16:40:30 -0300

    implementada calculadora em java

--- Java/Calculadora.java
+++ Java/Calculadora.java
@@ -4,9 +4,47 @@ public class Calculadora{
     public static void main(){
         Scanner scanner = new Scanner(System.in);
 
-        int a=0,b=0, op=0;
+        int op=0;
+        float som=0, a=0,b=0;
 
+        System.out.print("escolha a operacao:\n 1-soma\n 2-sub\n 3-div\n 4-mult\n");
+        op=scanner.nextInt();
         
+        System.out.print("Digite 1 numer: ");
+        a=scanner.nextInt();
+
+        System.out.print("digite outro: ");
+        b=scanner.nextInt();
+
+        if(op==1){
+            som=a+b;
+
+            System.out.printf("Sua soma: %.2\n", som);
+        }else if(op==2){
+            if(a>b){
+                som=a-b;
+            }else{
+                som=b-a;
+            }
+
+            System.out.printf("Sua sub: %.2f\n", som);
+        }else if(op==3){
+            if(b!=0){
+                som=a/b;
+                System.out.printf("Sua div: %f\n", som);
+            }else{
+                som=0;
+                System.out.printf("Divisão por 0 não suportada\n");
+            }
+        }else if(op==4){
+            som=a*b;
+
+            System.out.printf("Sua mult: %.2f\n", som);
+        }else{
+            System.out.printf("Escolha a operação\n");
+        }
+
+        scanner.close();
 
     }
 }
