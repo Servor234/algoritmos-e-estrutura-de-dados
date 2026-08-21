032ef1ee67e18213e894e9029164b37925d1cf9e
Renato Teixeira <teixeirarenato603@gmail.com>
2026-08-21 12:29:25 -0300

    Update Is.java

--- Java/Is.java
+++ Java/Is.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Is{
-    Boolean vowls(String a){
+    public static Boolean vowls(String a){
         for(int i=0;i<a.length();i++){
             if(a.charAt(i)!='a' ||a.charAt(i)!='e'||a.charAt(i)!='i'||a.charAt(i)!='o'||a.charAt(i)!='u'){
                 return false;
@@ -9,35 +9,70 @@ public class Is{
         }
         return true;
     } 
-    Boolean Nvowls(String a){
+    public static Boolean Nvowls(String a){
         for(int i=0;i<a.length();i++){
-            if(a.charAt(i)=='a' ||a.charAt(i)=='e'||a.charAt(i)=='i'||a.charAt(i)=='o'||a.charAt(i)=='u'){
+            if((a.charAt(i)=='a' ||a.charAt(i)=='e'||a.charAt(i)=='i'||a.charAt(i)=='o'||a.charAt(i)=='u') || (a.charAt(i)<(char)65)){
                 return false;
             }
         }
         return true;
     } 
 
-    Boolean intege(String a){
+    public static Boolean intege(String a){
         for(int i=0;i<a.length();i++){
-            if(a.charAt(i)=='.' && (a.charAt(i)<(char)48 && a.charAt(i)>(char)57)){
+            if(a.charAt(i)=='.' || (a.charAt(i)<(char)48 && a.charAt(i)>(char)57)){
                 return false;
             }
         }
         return true;
     }
 
-    Boolean rei(String a){
+    public static Boolean rei(String a){
         for(int i=0;i<a.length();i++){
-            if(a.charAt(i)=='.' && (a.charAt(i)>=(char)48 && a.charAt(i)<=(char)57)){
-                return true;
+            if((a.charAt(i)<(char)48 && a.charAt(i)>(char)57) || (a.charAt(i) != (char)46 || a.charAt(i)!=(char)44)){
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] args){
         Scanner lei = new Scanner(System.in);
+        String carlos;
+
+        carlos = lei.nextLine();
+
+        if(vowls(carlos)==true){
+            System.out.printf("SIM ");
+        }else{
+            System.out.printf("NAO ");
+        }
+
+        
+        if(Nvowls(carlos)==true){
+            System.out.printf("SIM ");
+        }else{
+            System.out.printf("NAO ");
+        }
+
+        
+        if(intege(carlos)==true){
+            System.out.printf("SIM ");
+        }else{
+            System.out.printf("NAO ");
+        }
+
+        
+        if(rei(carlos)==true){
+            System.out.printf("SIM ");
+        }else{
+            System.out.printf("NAO ");
+        }
+
+        System.out.printf("\n");
+
+        System.out.println("Carlinhos maia");
+
         lei.close();
     }
 }
