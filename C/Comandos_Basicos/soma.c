c852b32395516586a345920dec0bece800fb86ba
Renato Teixeira <teixeirarenato603@gmail.com>
2026-08-26 15:34:51 -0300

    Update soma.c

--- C/Comandos_Basicos/soma.c
+++ C/Comandos_Basicos/soma.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
+int soma(int a){
+	int soma2=0;
+	while(a!=0){
+		soma2+=a%10;
+		a/=10;
+	}
+
+	return soma2;
+}
+
 int main(){
-    int numero=0, soma=0;
+    int numero=0, soma1=0;
 
     //leitura do numero a ser somado
-    scanf("%d", &numero);
-
+    while(scanf("%d", &numero)!=EOF){
     //faz a soma dos restos da divisão e depois divide
-    while(numero!=0){
-        soma+=numero%10;
-        numero/=10;
-    }
+    soma1=soma(numero);
 
     //exibe a soma final
-    printf("%d\n", soma);
+    printf("%d\n", soma1);
+    }
 
 }
